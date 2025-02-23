2ae86b0226767a854d9304c540e4978e3f95e894
ZyroNex2004 <rohitkumar99421@gmail.com>
2025-02-23 22:41:23 +0530

    add passport

---
+++ models/user.js
@@ -0,0 +1,15 @@
+const mongoose = require("mongoose");
+const Schema = mongoose.Schema;
+const passportLocalMongoose = require("passport-local-mongoose");
+
+const userSchema = new Schema({
+    email:{
+        type: String,
+        required: true,
+      // passportlocalmongoose define kr deta hai username and password ko hash aur salt v add krta hai ye
+    }
+});
+
+User.plugin(passportLocalMongoose);
+
+module.exports = mongoose.model('User', userSchema);
