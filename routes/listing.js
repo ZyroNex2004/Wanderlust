0db15b01604c63d6e8cac7682e32ff6fd8961959
ZyroNex2004 <rohitkumar99421@gmail.com>
2025-02-28 14:14:35 +0530

    store file

--- routes/listing.js
+++ routes/listing.js
@@ -5,8 +5,10 @@ const Listing = require("../models/listing.js");
 const {isLoggedIn, isOwner, validateListing} = require("../middleware.js");
 
 const listingController = require("../controllers/listings.js");
-const multer  = require('multer')
-const upload = multer({ dest: 'uploads/' })
+const multer  = require("multer");
+const { storage } = require("../cloudConfig.js");
+
+const upload = multer({ storage });
 
 
     //index route
