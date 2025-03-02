3b1668ea40fb9fac8cb9aa76d06328658f309c60
ZyroNex2004 <rohitkumar99421@gmail.com>
2025-03-03 01:03:17 +0530

    add first map

---
+++ public/js/map.js
@@ -0,0 +1,12 @@
+
+
+ 
+  
+mapboxgl.accessToken = mapToken;
+
+const map =new mapboxgl.Map({
+  container: "map",
+  style: "mapbox://styles/mapbox/streets-v12",
+  center: [ 77.203613, 28.549507],
+  zoom: 9,
+});
