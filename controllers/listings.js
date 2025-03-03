c94e75cf78de23edbacae1c442315a64a0fe3903
ZyroNex2004 <rohitkumar99421@gmail.com>
2025-03-03 22:32:40 +0530

    add geocoding

--- controllers/listings.js
+++ controllers/listings.js
@@ -1,9 +1,10 @@
 const Listing = require("../models/listing");
-
-
+const mbxGeocoding = require('@mapbox/mapbox-sdk/services/geocoding');
+const mapToken = process.env.MAP_TOKEN;
+const geocodingClient = mbxGeocoding({ accessToken: mapToken });
 
 module.exports.index = async (req,res) => { // app ==> router
-    const allListings = await Listing.find({});         // /listings==> /
+    const allListings = await Listing.find({});         // /listings==> />
     res.render("listings/index.ejs",{allListings});
           
 };
@@ -28,6 +29,16 @@ module.exports.showListing = async (req, res) => {
 
 
 module.exports.createListing = async (req,res,next) => {
+  let response = await geocodingClient.forwardGeocode({
+        query: req.body.listing.location,
+        limit: 1,
+      })
+        .send();
+        
+     console.log(response.body.features[0].geometry);
+     res.send("done!");
+        
+
      let url = req.file.path;
      let filename = req.file.filename;
      
